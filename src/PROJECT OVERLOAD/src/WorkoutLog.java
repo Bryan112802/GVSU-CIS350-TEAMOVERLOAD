77373ec0afa75d856d52cd542cca03a4b5d84ed5
Bryan112802 <sorianob@mail.gvsu.edu>
2022-10-27 18:30:48 -0400

    Workoutlog update

--- src/PROJECT OVERLOAD/src/WorkoutLog.java
+++ src/PROJECT OVERLOAD/src/WorkoutLog.java
@@ -1,6 +1,27 @@
+package overload;
 import java.util.*;
 
 public class WorkoutLog {
-    private ArrayList<String> workout;
-    
+
+    private ArrayList<String> workouts;
+
+    private String date;
+
+    private String sets;
+
+
+    public void workouts() { // workout options array
+        workouts =  new ArrayList<>();
+        workouts.add("Squat");
+        workouts.add("Bench Press");
+        workouts.add("Deadlift");
+        workouts.add("Snatch");
+        workouts.add("Clean and Jerk");
+    }
+
+    public WorkoutLog() { // class instantiated
+
+    }
+
+
 }
