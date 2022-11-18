db156007e743c1f596d90c2936761dc0cddd9d16
Juan Hernandez <hernanjj@mail.gvsu.edu>
2022-11-18 13:55:09 -0500

    Implementing file/array functionality

--- src/PROJECT OVERLOAD/src/Main.java
+++ src/PROJECT OVERLOAD/src/Main.java
@@ -1,15 +1,6 @@
 
 import java.util.Scanner;
 
-
-
-
-
-
-
-
-
-
 public class Main extends LogHistory{
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
@@ -20,48 +11,51 @@ public class Main extends LogHistory{
         System.out.println("Select a task: ");
         System.out.println("    1) New Log");
         System.out.println("    2) Workout History");
-        System.out.println("    3) Exit");
+        System.out.println("    0) Exit");
         int initSelection = scnr.nextInt();
+        do {
+            if (initSelection == 1) {
+                System.out.println("Select your exercise: ");
+                System.out.println("    1) Squat");
+                System.out.println("    2) Bench Press");
+                System.out.println("    3) Deadlift");
+                System.out.println("    4) Snatch");
+                System.out.println("    5) Clean and Jerk");
 
-        if (initSelection == 1) {
-            System.out.println("Select your exercise: ");
-            System.out.println("    1) Squat");
-            System.out.println("    2) Bench Press");
-            System.out.println("    3) Deadlift");
-            System.out.println("    4) Snatch");
-            System.out.println("    5) Clean and Jerk");
+                WorkoutLog newLog = new WorkoutLog();
 
-            WorkoutLog newLog = new WorkoutLog();
+                int exerciseSelection;
+                exerciseSelection = scnr.nextInt();
 
-            int exerciseSelection;
-            exerciseSelection = scnr.nextInt();
+                if (exerciseSelection == 1) {
+                    newLog.addWorkout(1);
+                    System.out.println("You have selected squat!");
+                }
+                else if (exerciseSelection == 2) {
+                    newLog.addWorkout(2);
+                    System.out.println("You have selected bench press!");
+                }
+                else if (exerciseSelection == 3) {
+                    newLog.addWorkout(3);
+                    System.out.println("You have selected deadlift!");
+                }
+                else if (exerciseSelection == 4) {
+                    newLog.addWorkout(4);
+                    System.out.println("You have selected snatch!");
+                }
+                else if (exerciseSelection == 5) {
+                    newLog.addWorkout(5);
+                    System.out.println("You have selected clean and jerk!");
+                }
+                else {
+                    //make this throw exception
+                    System.out.println("Invalid Response");
+                }
 
-            if (exerciseSelection == 1) {
-                newLog.addWorkout(1);
-                System.out.println("You have selected squat!");
-            }
-            else if (exerciseSelection == 2) {
-                newLog.addWorkout(2);
-                System.out.println("You have selected bench press!");
-            }
-            else if (exerciseSelection == 3) {
-                newLog.addWorkout(3);
-                System.out.println("You have selected deadlift!");
-            }
-            else if (exerciseSelection == 4) {
-                newLog.addWorkout(4);
-                System.out.println("You have selected snatch!");
-            }
-            else if (exerciseSelection == 5) {
-                newLog.addWorkout(5);
-                System.out.println("You have selected clean and jerk!");
-            }
-            else {
-                //make this throw exception
-                System.out.println("Invalid Response");
-            }
 
+            }
         }
+        while (initSelection != 0);
 
 
     }
